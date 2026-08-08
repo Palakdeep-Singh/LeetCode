3edbf541c6c28aaa87ea1afce949e8077f844ba4
Palakdeep-Singh <palakdeep8055@gmail.com>
2026-08-08 16:08:21 +0530

    fixed duplicate value search

--- Binary_Search/search_rotated_array.cpp
+++ Binary_Search/search_rotated_array.cpp
@@ -8,30 +8,37 @@ int bs(vector<int> &nums, int target)
 {
 
   int l = 0, h = nums.size() - 1;
-  int ans = -1;
+
   while (l <= h)
   {
     int mid = l + (h - l) / 2;
 
+    if (nums[mid] == target)
+    {
+      return 1;
+    }
+
+    // in case of duplicates we will just return true,false (1,0) if element is present or not.
+    // we cannot return first occurence in log(n) for duplicates. for that we have to perform linear search
+
+    if (nums[l] == nums[mid] && nums[mid] == nums[h])
+    {
+      l++;
+      h--;
+      continue;
+    }
     // now check wether the first half is sorted or second half.
-    if (nums[l] < nums[mid]) // if first is sorted. nums[low] <= nums[mid].
+    if (nums[l] <= nums[mid]) // if first is sorted. nums[low] <= nums[mid].
     {
       // check wether the target lies in that sorted half or not
       if (nums[l] <= target && target <= nums[mid])
       {
         h = mid - 1;
-        ans = mid;
       }
       else
       {
         l = mid + 1;
       }
-    }
-    else if (nums[l] == nums[mid] && nums[mid] == nums[h])
-    {
-      l++;
-      h--;
-      continue;
     }
     else
     {
@@ -45,22 +52,17 @@ int bs(vector<int> &nums, int target)
       {
         h = mid - 1;
       }
-
-      if (nums[mid] == target)
-      {
-        ans = mid;
-      }
     }
   }
 
-  return ans; // in case of no element is found.
+  return -1; // in case of no element is found.
 }
 
 int main()
 {
 
-  vector<int> nums = {7, 8, 1, 3, 3, 3, 3, 4, 5, 6};
+  vector<int> nums = {3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
 
-  cout << bs(nums, 3) << endl;
+  cout << bs(nums, 1) << endl;
   return 0;
 }
