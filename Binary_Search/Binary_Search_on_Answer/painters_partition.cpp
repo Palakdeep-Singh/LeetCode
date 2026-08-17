569b0f757ed020acb712e89f9c2fc56ec62da6ba
Palakdeep-Singh <palakdeep8055@gmail.com>
2026-08-17 14:28:49 +0530

    Painter's problem solved using same approach of book allocation

---
+++ Binary_Search/Binary_Search_on_Answer/painters_partition.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// problem states that minimize the largest sum of subarray where k non-zero subarray's can be made by splitting the array.
+
+long long condition(vector<int> &a, int mid)
+{
+
+  long long total = 0;
+  int count = 1;
+  for (int i = 0; i < a.size(); i++)
+  {
+    total += a[i];
+
+    if (total > mid)
+    {
+      total = a[i];
+      count++;
+    }
+  }
+
+  return count;
+}
+
+long long binary_search(vector<int> &arr, int k)
+{
+
+  if (k > arr.size())
+    return -1;
+
+  long long low = *max_element(arr.begin(), arr.end());
+  long long high = accumulate(arr.begin(), arr.end(), 0);
+
+  while (low < high)
+  {
+
+    long long mid = low + (high - low) / 2;
+
+    if (condition(arr, mid) <= k)
+    {
+      high = mid;
+    }
+    else
+    {
+      low = mid + 1;
+    }
+  }
+
+  return high;
+}
+
+int main()
+{
+
+  vector<int> arr = {10, 20, 20, 340, 460, 600};
+  int k = 3;
+
+  cout << binary_search(arr, k) << endl;
+
+  return 0;
+}
